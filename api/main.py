501d176cdcd90230a9cdcb377d5e5bab0bdc91d5
Muralikrishnaraparthi <muralikrishnaraparthi@gmail.com>
2025-08-01 00:40:47 +0530

    Fixed the logger to trace issues with insert

--- api/main.py
+++ api/main.py
@@ -35,6 +35,7 @@ logger = logging.getLogger(__name__)
 logger.info(f"Using SQLITE_DB_PATH: {SQLITE_DB_PATH}")
 logger.info(f"Using LOG_FILE_PATH: {LOG_FILE_PATH}")
 
+
 # --- SQLite Logging ---
 def log_to_sqlite(input_data: dict, output_data: list):
     try:
